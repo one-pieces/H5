48fc22ed616e3f30eb8c330b1a6112886c3cc6af
林小龙 <xiaolong.lin@ele.me>
2017-08-13 23:42:58 +0800

    feat: wechat share config

--- nissan-jinke/js/script.js
+++ nissan-jinke/js/script.js
@@ -293,7 +293,99 @@ define(['jquery', 'resLoader', 'weixin'], function ($, resLoader, wx) {
   }
 
   self.share = function () {
+    $.ajax({
+      type: 'post',
+      url: 'WeixinJSSDKServlet?type=getWxConfig',
+      data: { url: window.location.href },
+      dataType: 'json',
+      success: function (args) {
+        wx.config({
+          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          appId: args.appId, // 必填，公众号的唯一标识
+          timestamp: args.timestamp, // 必填，生成签名的时间戳
+          nonceStr: args.nonceStr, // 必填，生成签名的随机串
+          signature: args.signature,// 必填，签名，见附录1
+          jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareWeibo', 'onMenuShareQZone', 'scanQRCode'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+        });
+        wx.ready(function () {
+          var url = window.location.href,
+            title = '极限驾控营',
+            desc = '全国24城 等你开飙',
+            imgUrl = 'http://tron-m.com/long/assets/img/loading/0.png';
 
+          wx.onMenuShareTimeline({
+            title: '极限驾控营，全国24城 等你开飙', // 分享标题
+            desc: '', // 分享描述
+            link: url, // 分享链接
+            imgUrl: imgUrl, // 分享图标
+            success: function () {
+              // 用户确认分享后执行的回调函数
+            },
+            cancel: function () {
+              // 用户取消分享后执行的回调函数
+            }
+          });
+
+          wx.onMenuShareAppMessage({
+            title: title, // 分享标题
+            desc: desc, // 分享描述
+            link: url, // 分享链接
+            imgUrl: imgUrl, // 分享图标
+            type: '', // 分享类型,music、video或link，不填默认为link
+            dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+            success: function () {
+              // 用户确认分享后执行的回调函数
+            },
+            cancel: function () {
+              // 用户取消分享后执行的回调函数
+            }
+          });
+
+          wx.onMenuShareQQ({
+            title: title, // 分享标题
+            desc: desc, // 分享描述
+            link: url, // 分享链接
+            imgUrl: imgUrl, // 分享图标
+            success: function () {
+              // 用户确认分享后执行的回调函数
+            },
+            cancel: function () {
+              // 用户取消分享后执行的回调函数
+            }
+          });
+
+          wx.onMenuShareWeibo({
+            title: title, // 分享标题
+            desc: desc, // 分享描述
+            link: url, // 分享链接
+            imgUrl: imgUrl, // 分享图标
+            success: function () {
+              // 用户确认分享后执行的回调函数
+            },
+            cancel: function () {
+              // 用户取消分享后执行的回调函数
+            }
+          });
+
+          wx.onMenuShareQZone({
+            title: title, // 分享标题
+            desc: desc, // 分享描述
+            link: url, // 分享链接
+            imgUrl: imgUrl, // 分享图标
+            success: function () {
+              // 用户确认分享后执行的回调函数
+            },
+            cancel: function () {
+              // 用户取消分享后执行的回调函数
+            }
+          });
+        });
+
+        wx.error(function (res) {
+          console.log("wx has error:" + res);
+        });
+      }
+    });
   }
 
   function confirmUserInfo() {
@@ -380,5 +472,7 @@ define(['jquery', 'resLoader', 'weixin'], function ($, resLoader, wx) {
   function hidePage(id) {
     self.pages[id].hide();
   }
+
+  self.share();
   return self;
 });
