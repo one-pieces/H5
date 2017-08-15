380e2c0d8942d3b50b286dc685792f936448450c
林小龙 <xiaolong.lin@ele.me>
2017-08-15 22:40:09 +0800

    fix: 微信分享config获取错误

--- nissan-jinke/js/script.js
+++ nissan-jinke/js/script.js
@@ -302,15 +302,20 @@ define(['jquery', 'resLoader', 'weixin'], function ($, resLoader, wx) {
       url: 'http://www.tron-m.com/tron-api/jssdk/share.do',
       data: { url: window.location.href },
       dataType: 'json',
-      success: function (args) {
-        wx.config({
-          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-          appId: args.appId, // 必填，公众号的唯一标识
-          timestamp: args.timestamp, // 必填，生成签名的时间戳
-          nonceStr: args.nonceStr, // 必填，生成签名的随机串
-          signature: args.signature,// 必填，签名，见附录1
-          jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareWeibo', 'onMenuShareQZone', 'scanQRCode'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-        });
+      success: function (json) {
+        if (json.code == 1) {
+          var result = json.result;
+          wx.config({
+            debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            appId: result.appId, // 必填，公众号的唯一标识
+            timestamp: result.timestamp, // 必填，生成签名的时间戳
+            nonceStr: result.nonceStr, // 必填，生成签名的随机串
+            signature: result.signature,// 必填，签名，见附录1
+            jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareWeibo', 'onMenuShareQZone', 'scanQRCode'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+          });
+        } else {
+          console.log("getWXConfig has error:" + json.msg);
+        }
         wx.ready(function () {
           var url = window.location.href,
             title = '极限驾控营',
