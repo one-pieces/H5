e7a58eb7fdd73e6609b6f9bbb8e50eb3222fa68c
林小龙 <xiaolong.lin@ele.me>
2017-09-20 20:10:04 +0800

    feat: add page2

--- luoshi/js/script.js
+++ luoshi/js/script.js
@@ -1,14 +1,20 @@
 define(['jquery', 'createjs', 'View', 'Swiper'], function ($, createjs, View, Swiper) {
   var self = {
-    stage: null,
-    container: null
+    homePage: {
+      stage: null,
+      container: null
+    },
+    mainPage: {
+      stage: null,
+      container: null
+    }
   };
 
   self.open = function() {
     console.log('start', createjs);
     //放置静态资源的数组
     var manifest = [
-      {src: './assets/img/page1/arrow.png', id: 'page1_arrow'},
+      // page1
       {src: './assets/img/page1/01-title.png', id: 'page1_01-title'},
       {src: './assets/img/page1/02-english-title.png', id: 'page1_02-english-title'},
       {src: './assets/img/page1/03-guihua.png', id: 'page1_03-guihua'},
@@ -24,7 +30,31 @@ define(['jquery', 'createjs', 'View', 'Swiper'], function ($, createjs, View, Sw
       {src: './assets/img/page1/08-kongmingdeng/03.png', id: 'page1_08-kongmingdeng_03'},
       {src: './assets/img/page1/08-kongmingdeng/04.png', id: 'page1_08-kongmingdeng_04'},
       {src: './assets/img/page1/09-background.png', id: 'page1_09-background'},
-      {src: './assets/img/page1/10-wuding.png', id: 'page1_10-wuding'}
+      {src: './assets/img/page1/10-wuding.png', id: 'page1_10-wuding'},
+      // page2
+      {src: './assets/img/page2/00-background.jpg', id: 'page2_00-background'},
+      // {src: './assets/img/page2/01-guihua.png', id: 'page2_01-guihua'},
+      {src: './assets/img/page2/02-deng.png', id: 'page2_02-deng'},
+      {src: './assets/img/page2/03-hehua.png', id: 'page2_03-hehua'},
+      {src: './assets/img/page2/04-lianhua.png', id: 'page2_04-lianhua'},
+      {src: './assets/img/page2/05-kongmingdeng.png', id: 'page2_05-kongmingdeng'},
+      {src: './assets/img/page2/06-kongmingdeng.png', id: 'page2_06-kongmingdeng'},
+      {src: './assets/img/page2/07-kongmingdeng.png', id: 'page2_07-kongmingdeng'},
+      {src: './assets/img/page2/08-kongmingdeng.png', id: 'page2_08-kongmingdeng'},
+      {src: './assets/img/page2/09-kongmingdeng.png', id: 'page2_09-kongmingdeng'},
+      {src: './assets/img/page2/10-stars.png', id: 'page2_10-stars'},
+      // {src: './assets/img/page2/11-stars.png', id: 'page2_11-stars'},
+      // {src: './assets/img/page2/12-stars.png', id: 'page2_12-stars'},
+      // {src: './assets/img/page2/13-stars.png', id: 'page2_13-stars'},
+      {src: './assets/img/page2/14-petal.png', id: 'page2_14-petal'},
+      {src: './assets/img/page2/15-petal.png', id: 'page2_15-petal'},
+      {src: './assets/img/page2/16-petal.png', id: 'page2_16-petal'},
+      {src: './assets/img/page2/17-petal.png', id: 'page2_17-petal'},
+      {src: './assets/img/page2/18-petal.png', id: 'page2_18-petal'},
+      {src: './assets/img/page2/19-petal.png', id: 'page2_19-petal'},
+      {src: './assets/img/page2/20-wenzi1.png', id: 'page2_20-wenzi1'},
+      {src: './assets/img/page2/21-wenzi2.png', id: 'page2_21-wenzi2'},
+      {src: './assets/img/page2/22-wenzi3.png', id: 'page2_22-wenzi3'},
     ];
     window.queue = new createjs.LoadQueue();
     queue.on('progress', handleProgress);
@@ -32,8 +62,6 @@ define(['jquery', 'createjs', 'View', 'Swiper'], function ($, createjs, View, Sw
     queue.loadManifest(manifest);
     //资源加载成功后,进行处理
     function handleComplete(event) {
-      // var tyre = queue.getResult('page1_01-title');  //获取加载资源
-      // console.log(tyre);
       event.currentTarget.removeEventListener("progress", handleProgress);
       event.currentTarget.removeEventListener("complete", handleComplete);
 
@@ -43,41 +71,54 @@ define(['jquery', 'createjs', 'View', 'Swiper'], function ($, createjs, View, Sw
         paginationClickable: true,
         direction: 'vertical',
         onInit: function(swiper) {
-          console.log('aaaaaa');
           // swiperAnimateCache(swiper);
           // swiperAnimate(swiper);
+          self.initHomePage();
         },
         onSlideChangeEnd: function(swiper) {
           // swiperAnimate(swiper);
+          self.initMainPage();
         }
-//      onSlideChangeStart: function(swiper) {
-//        if (swiper.activeIndex == 3) { createSwiper2(); }
-//      }
       });
-
-      this.init();
+      // self.initMainPage();
     }
     function handleProgress(event) {
       console.log((queue.progress*100|0) + '%');
     }
   }
 
-  self.init = function() {
-    var canvas = document.getElementById('mainView');
-    this.stage = new createjs.Stage(canvas);
-    this.container = new createjs.Container();
-    this.stage.addChild(this.container);
-    createjs.Touch.enable(this.stage);
+  self.initHomePage = function() {
+    var canvas = document.getElementById('homeView');
+    this.homePage.stage = new createjs.Stage(canvas);
+    this.homePage.container = new createjs.Container();
+    this.homePage.stage.addChild(this.homePage.container);
+    createjs.Touch.enable(this.homePage.stage);
 
     createjs.Ticker.timingMode =  createjs.Ticker.RAF_SYNCHED;
     createjs.Ticker.setFPS(30);
-    createjs.Ticker.addEventListener('tick', this.stage);
+    createjs.Ticker.addEventListener('tick', this.homePage.stage);
     // createjs.Ticker.addEventListener("tick", stageBreakHandler);
 
     var contentView1 = new View.ContentView1();
-    // console.log(contentView1);
-    this.container.addChild(contentView1);
-    this.stage.update();
+    this.homePage.container.addChild(contentView1);
+    this.homePage.stage.update();
+  }
+
+  self.initMainPage = function() {
+    var canvas = document.getElementById('mainView');
+    this.mainPage.stage = new createjs.Stage(canvas);
+    this.mainPage.container = new createjs.Container();
+    this.mainPage.stage.addChild(this.mainPage.container);
+    createjs.Touch.enable(this.mainPage.stage);
+
+    createjs.Ticker.timingMode =  createjs.Ticker.RAF_SYNCHED;
+    createjs.Ticker.setFPS(30);
+    createjs.Ticker.addEventListener('tick', this.mainPage.stage);
+    // createjs.Ticker.addEventListener("tick", stageBreakHandler);
+
+    var contentView2 = new View.ContentView2();
+    this.mainPage.container.addChild(contentView2);
+    this.mainPage.stage.update();
   }
 
   return self;
--- luoshi/js/view.js
+++ luoshi/js/view.js
@@ -13,11 +13,9 @@
   var View = {};
 
   //ContentView1
-  (function() {
+  View.ContentView1 = (function() {
     function ContentView1() {
       this.Container_constructor();
-      console.log(queue.getResult('page1_09-background'));
-
       var mc = new createjs.MovieClip();
 
       // 兔子
@@ -43,21 +41,15 @@
         .to({rotation: -5}, 10)
         .to({rotation: 0}, 10));
 
-      // 箭头
-      var arrow = new createjs.Bitmap(queue.getResult('page1_arrow'));
-      arrow.setTransform(430, 1250);
-      arrow.rotation = 180;
-      mc.timeline.addTween(createjs.Tween.get(arrow, {loop: true}).to({y: 1300}, 30));
-
       // 灯笼
       var denglong = new createjs.Bitmap(queue.getResult('page1_05-denglong'));
       denglong.setTransform(590, 790);
       var denglongShadow = new createjs.Shadow('#ffd742', 0, 0, 40);
       denglong.shadow = denglongShadow;
       mc.timeline.addTween(createjs.Tween.get(denglongShadow, {loop: true})
-        .to({offsetX: 0, offsetY: 0, blur: 40})
-        .to({offsetX: 0, offsetY: 0, blur: 100}, 60)
-        .to({offsetX: 0, offsetY: 0, blur: 40}, 60));
+        .to({offsetX: 0, offsetY: 0, blur: 30})
+        .to({offsetX: 0, offsetY: 0, blur: 160}, 40)
+        .to({offsetX: 0, offsetY: 0, blur: 30}, 40));
       mc.timeline.addTween(createjs.Tween.get(denglong));
 
       // 屋顶
@@ -101,7 +93,7 @@
 
       // 桂花
       var guihua = new createjs.Bitmap(queue.getResult('page1_03-guihua'));
-      guihua.setTransform(-20, 1050);
+      guihua.setTransform(-20, 1090);
       guihua.regX = 0;
       guihua.regY = 190;
       mc.timeline.addTween(createjs.Tween.get(guihua, {loop: true})
@@ -111,7 +103,7 @@
         .to({rotation: 6}, 40)
         .to({rotation: 8}, 40, createjs.Ease.quadInOut));
       var guihua1 = new createjs.Bitmap(queue.getResult('page1_03-guihua'));
-      guihua1.setTransform(10, 1030);
+      guihua1.setTransform(10, 1070);
       guihua1.regX = 0;
       guihua1.regY = 190;
       mc.timeline.addTween(createjs.Tween.get(guihua1, {loop: true})
@@ -120,28 +112,28 @@
         .to({rotation: -10}, 40, createjs.Ease.quadInOut)
         .to({rotation: -8}, 40)
         .to({rotation: -10}, 40, createjs.Ease.quadInOut));
-      var guihua2 = new createjs.Bitmap(queue.getResult('page1_03-guihua'));
-      guihua2.setTransform(-30, 1080);
-      guihua2.regX = 0;
-      guihua2.regY = 190;
-      mc.timeline.addTween(createjs.Tween.get(guihua2, {loop: true})
-        .to({rotation: 15})
-        .to({rotation: 18}, 40)
-        .to({rotation: 15}, 40, createjs.Ease.quadInOut)
-        .to({rotation: 13}, 40)
-        .to({rotation: 15}, 40, createjs.Ease.quadInOut));
-      var guihua3 = new createjs.Bitmap(queue.getResult('page1_03-guihua'));
-      guihua3.setTransform(-50, 1050);
-      guihua3.regX = 0;
-      guihua3.regY = 190;
-      mc.timeline.addTween(createjs.Tween.get(guihua3, {loop: true})
-        .to({rotation: 40})
-        .to({rotation: 42}, 40)
-        .to({rotation: 40}, 40, createjs.Ease.quadInOut)
-        .to({rotation: 38}, 40)
-        .to({rotation: 40}, 40, createjs.Ease.quadInOut));
+      // var guihua2 = new createjs.Bitmap(queue.getResult('page1_03-guihua'));
+      // guihua2.setTransform(-30, 1080);
+      // guihua2.regX = 0;
+      // guihua2.regY = 190;
+      // mc.timeline.addTween(createjs.Tween.get(guihua2, {loop: true})
+      //   .to({rotation: 15})
+      //   .to({rotation: 18}, 40)
+      //   .to({rotation: 15}, 40, createjs.Ease.quadInOut)
+      //   .to({rotation: 13}, 40)
+      //   .to({rotation: 15}, 40, createjs.Ease.quadInOut));
+      // var guihua3 = new createjs.Bitmap(queue.getResult('page1_03-guihua'));
+      // guihua3.setTransform(-50, 1050);
+      // guihua3.regX = 0;
+      // guihua3.regY = 190;
+      // mc.timeline.addTween(createjs.Tween.get(guihua3, {loop: true})
+      //   .to({rotation: 40})
+      //   .to({rotation: 42}, 40)
+      //   .to({rotation: 40}, 40, createjs.Ease.quadInOut)
+      //   .to({rotation: 38}, 40)
+      //   .to({rotation: 40}, 40, createjs.Ease.quadInOut));
       var guihua4 = new createjs.Bitmap(queue.getResult('page1_03-guihua'));
-      guihua4.setTransform(5, 1000);
+      guihua4.setTransform(5, 1040);
       guihua4.regX = 0;
       guihua4.regY = 190;
       mc.timeline.addTween(createjs.Tween.get(guihua4, {loop: true})
@@ -151,7 +143,7 @@
         .to({rotation: -3}, 40)
         .to({rotation: -5}, 40, createjs.Ease.quadInOut));
       var guihua5 = new createjs.Bitmap(queue.getResult('page1_03-guihua'));
-      guihua5.setTransform(-10, 980);
+      guihua5.setTransform(-10, 1020);
       guihua5.regX = 0;
       guihua5.regY = 190;
       mc.timeline.addTween(createjs.Tween.get(guihua5, {loop: true})
@@ -197,7 +189,136 @@
       this.addChild(this.content);
     }
     createjs.extend(ContentView1, createjs.Container);
-    View.ContentView1 = createjs.promote(ContentView1, 'Container');
+    return createjs.promote(ContentView1, 'Container');
+  }());
+
+  //ContentView2
+  View.ContentView2 = (function() {
+    function ContentView2() {
+      this.Container_constructor();
+      var mc = new createjs.MovieClip();
+
+      // 花瓣
+      // [
+      //   ['page2_14-petal', 2250, 1050],
+      //   ['page2_15-petal', 2150, 930],
+      //   ['page2_16-petal', 1920, 887],
+      //   ['page2_17-petal', 1800, 810],
+      //   ['page2_18-petal', 1740, 728],
+      //   ['page2_19-petal', 1640, 668]
+      // ].forEach(function(item) {
+      //   var kongmingdeng = new createjs.Bitmap(queue.getResult(item[0]));
+      //   kongmingdeng.setTransform(item[1], item[2]);
+      //   if (item[3]) {
+      //     kongmingdeng.scaleX = item[3];
+      //     kongmingdeng.scaleY = item[3];
+      //   }
+      //   mc.timeline.addTween(createjs.Tween.get(kongmingdeng));
+      // });
+
+      // 孔明灯
+      [
+        ['page2_05-kongmingdeng', 2250, 250],
+        ['page2_05-kongmingdeng', 250, 230, 0.6],
+        ['page2_06-kongmingdeng', 1520, 187, 0.4],
+        ['page2_07-kongmingdeng', 2200, 110],
+        ['page2_08-kongmingdeng', 2040, 128],
+        ['page2_09-kongmingdeng', 340, 168]
+      ].forEach(function(item) {
+        var kongmingdeng = new createjs.Bitmap(queue.getResult(item[0]));
+        kongmingdeng.setTransform(item[1], item[2]);
+        if (item[3]) {
+          kongmingdeng.scaleX = item[3];
+          kongmingdeng.scaleY = item[3];
+        }
+        mc.timeline.addTween(createjs.Tween.get(kongmingdeng));
+      });
+
+      // 灯
+      [[700, 590], [820, 787], [1500, 610], [1640, 768]].forEach(function(item) {
+        var deng = new createjs.Bitmap(queue.getResult('page2_02-deng'));
+        deng.setTransform(item[0], item[1]);
+        var dengShadow = new createjs.Shadow('#FCF0BC', 0, 0, 16);
+        deng.shadow = dengShadow;
+        mc.timeline.addTween(createjs.Tween.get(dengShadow, {loop: true})
+          .to({offsetX: 0, offsetY: 0, blur: 100}, 90, createjs.Ease.quadInOut)
+          .to({offsetX: 0, offsetY: 0, blur: 16}, 90, createjs.Ease.quadInOut));
+        mc.timeline.addTween(createjs.Tween.get(deng));
+      });
+
+      // 星星
+      var stars = new createjs.Bitmap(queue.getResult('page2_10-stars'));
+      mc.timeline.addTween(createjs.Tween.get(stars, {loop: true}).to({alpha: 0.2}, 20).to({alpha: 1}, 20));
+
+      // 荷花
+      var hehua = new createjs.Bitmap(queue.getResult('page2_03-hehua'));
+      hehua.setTransform(420, 1147);
+      hehua.scaleX = 1.5;
+      hehua.scaleY = 1.5;
+      mc.timeline.addTween(createjs.Tween.get(hehua, {loop: true})
+        .to({y: 1140}, 30).to({y: 1147}, 30));
+      var hehua1 = new createjs.Bitmap(queue.getResult('page2_03-hehua'));
+      hehua1.setTransform(1720, 947);
+      hehua1.scaleX = 0.9;
+      hehua1.scaleY = 0.9;
+      mc.timeline.addTween(createjs.Tween.get(hehua1, {loop: true})
+        .to({y: 940}, 30).to({y: 947}, 30).wait(5));
+
+      // 莲花
+      var lianhua = new createjs.Bitmap(queue.getResult('page2_04-lianhua'));
+      lianhua.setTransform(920, 1047);
+      lianhua.scaleX = 0.8;
+      lianhua.scaleY = 0.8;
+      mc.timeline.addTween(createjs.Tween.get(lianhua, {loop: true})
+        .to({y: 1040}, 30).to({y: 1047}, 30));
+      var lianhua1 = new createjs.Bitmap(queue.getResult('page2_04-lianhua'));
+      lianhua1.setTransform(1520, 927);
+      lianhua1.scaleX = 0.5;
+      lianhua1.scaleY = 0.5;
+      mc.timeline.addTween(createjs.Tween.get(lianhua1, {loop: true})
+        .to({x: 1518, y: 920}, 30).to({x: 1520, y: 927}, 30));
+
+      // 背景
+      var background = new createjs.Bitmap(queue.getResult('page2_00-background'));
+      mc.timeline.addTween(createjs.Tween.get(background));
+
+      mc.gotoAndPlay('start');
+
+      var cmc = new createjs.MovieClip();
+
+      // 文字
+      var wenzi1 = new createjs.Bitmap(queue.getResult('page2_20-wenzi1'));
+      wenzi1.setTransform(200, 200);
+      var wenzi2 = new createjs.Bitmap(queue.getResult('page2_21-wenzi2'));
+      wenzi2.setTransform(250, 200);
+      wenzi2.alpha = 0;
+      var wenzi3 = new createjs.Bitmap(queue.getResult('page2_22-wenzi3'));
+      wenzi3.setTransform(330, 200);
+      wenzi3.alpha = 0;
+      cmc.timeline.addTween(createjs.Tween.get(wenzi1).to({alpha: 0}, 60));
+      cmc.timeline.addTween(createjs.Tween.get(wenzi2).wait(60).to({alpha: 1}, 60).to({alpha: 0}, 60));
+      cmc.timeline.addTween(createjs.Tween.get(wenzi3).wait(180).to({alpha: 1}, 60).to({alpha: 0}, 60));
+
+      var content = new createjs.Container();
+      content.addChild(mc);
+      cmc.timeline.addTween(createjs.Tween.get(content)
+        .to({x: -100})
+        .to({x: -1900}, 300, createjs.Ease.sineInOut).wait(20)
+        .to({x: -980}, 150, createjs.Ease.sineInOut).wait(50)
+        // .to({x: -1900}, 300, createjs.Ease.sineIn).wait(20)
+        // .to({x: -980}, 150, createjs.Ease.sineIn).wait(20)
+        .to({regX: 1375, regY: 0, scaleX: 5, scaleY: 5, alpha: 0}, 60)
+        .call(function() {
+          console.log('aaaaa');
+        }));
+
+      cmc.gotoAndPlay('start');
+
+      this.content = cmc;
+      this.addChild(this.content);
+    }
+    createjs.extend(ContentView2, createjs.Container);
+    return createjs.promote(ContentView2, 'Container');
   }());
   return View;
 }));
