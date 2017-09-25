cccf581299ad772f481c97eda96b17fec66e2a5a
林小龙 <xiaolong.lin@ele.me>
2017-09-25 12:43:04 +0800

    test

--- luoshi/js/script.js
+++ luoshi/js/script.js
@@ -371,7 +371,7 @@ define(['jquery', 'createjs', 'View', 'Swiper', 'weixin'], function ($, createjs
     //   console.log("getWXConfig has error:" + json.msg);
     // }
     wx.config({
-      debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+      debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
       appId: 'wx9c52c1ab1061abfe', // 必填，公众号的唯一标识
       timestamp: '1506314081', // 必填，生成签名的时间戳
       nonceStr: 'b6e51204-6bda-43b4-b239-466dbaaad3b1', // 必填，生成签名的随机串
